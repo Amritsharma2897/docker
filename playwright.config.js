51209f27bc643a36b3ace86f1974e2ca7ed2a02d
Amritsharma2897 <amrit.sharma@clanap.com>
2024-01-29 16:08:28 +0530

    updated spec file and yml file

--- playwright.config.js
+++ playwright.config.js
@@ -12,7 +12,7 @@ const { defineConfig, devices } = require('@playwright/test');
  */
 module.exports = defineConfig({
   testDir: './tests',
-  reporter: process.env.CI ? '[blob,html]' : 'html',
+  reporter: process.env.CI ? 'blob' : 'html',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
