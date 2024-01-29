42b06c2de200bd8b27709c4f6dfce5260f937c9e
Amritsharma2897 <amrit.sharma@clanap.com>
2024-01-29 15:47:58 +0530

    generate blob and html report

--- playwright.config.js
+++ playwright.config.js
@@ -12,13 +12,13 @@ const { defineConfig, devices } = require('@playwright/test');
  */
 module.exports = defineConfig({
   testDir: './tests',
-  reporter: process.env.CI ? 'blob' : 'html',
+  reporter: process.env.CI ? 'blob,html' : 'html',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 3 : 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
